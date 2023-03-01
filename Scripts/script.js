d55cba58a4739f5d1aebc85322b368911bd37b6c
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-03-01 14:23:43 +0530

    Typed Library added

---
+++ Scripts/script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var types = new Typed('.typing',{
+    strings : ["Genius","Freelancer"," Full Stack Web Developer","Designer"],
+    typeSpeed : 100 ,
+    backSpeed: 60,
+    loop : true
+})
