22b6489695d90b98fb88eeb97a1c42081c0aa3e1
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-03-01 16:23:16 +0530

    formated style part

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,15 @@
 // typing animation
 
-var types = new Typed('.typing',{
-    strings : ["Genius","Freelancer"," Full Stack Web Developer","Designer"],
-    typeSpeed : 100 ,
+var types = new Typed('.typing', {
+    strings: ["Genius", "Freelancer", " Full Stack Web Developer", "Designer"],
+    typeSpeed: 100,
     backSpeed: 60,
-    loop : true
-})
+    loop: true
+});
+
+var types = new Typed('.typing-2', {
+    strings: ["Genius", "Freelancer", " Full Stack Web Developer", "Designer"],
+    typeSpeed: 99,
+    backSpeed: 58,
+    loop: true
+});
